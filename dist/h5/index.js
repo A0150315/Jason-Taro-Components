150ab87659f72a6b75a0bbf486436afdc9b440dd
谭健青 <574469551@qq.com>
2019-09-02 16:48:45 +0800

    update:demo

--- dist/h5/index.js
+++
@@ -1,4 +0,0 @@
-import Taro from "@tarojs/taro-h5";
-Taro.initPxTransform({ designWidth: 750 });
-export { default as Tab } from './components/tab';
-export { default as TabPanel } from './components/tab/tab-panel';
